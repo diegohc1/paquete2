b507a6dfc4bc234a9e2231abf5d1c4d36bd92794
Diego Hermoza <dhermoza.c@gmail.com>
2022-05-23 17:45:55 -0500

    intento de paquete1

---
+++ R/tabla_freq.R
@@ -0,0 +1,37 @@
+
+#' tabla freq
+#'
+#' Lo mismo que table() pero devuelve un dataframe y acepta pesos
+#'
+#' @param data Dataframe
+#' @param x Variable categórica
+#' @param peso En caso pesos
+#'
+#' @return Dataframe
+#' @export
+#'
+#' @examples
+#' tabla_freq(mtcars, "gear")
+#'
+#'
+
+tabla_freq <- function(data, x, peso = NULL){
+
+  if(!is.null(peso)){
+    f <- paste0(peso, "~", x)
+    df1 <- aggregate(formula = as.formula(f), data = data, FUN = sum)
+    names(df1)[names(df1) == peso] <- "Freq"
+  }else{
+    df1 <- as.data.frame(table(data[x]))
+  }
+
+  df2 <- within(df1, {prop = round(prop.table(Freq)*100, 1)})
+  names(df2) <- c("opcion", "n", "prop")
+
+  return(df2)
+
+}
+
+
+
+
