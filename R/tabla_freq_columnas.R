d3fd146c65f1696e7e9f9aee5ef4f1f54cbd18c8
Diego Hermoza <dhermoza.c@gmail.com>
2022-07-14 18:00:03 -0500

    agregando tabla_freq_x

---
+++ R/tabla_freq_columnas.R
@@ -0,0 +1,32 @@
+#' Tabla de frecuencias con varias columnas (Frequency table with many columns)
+#'
+#' Aplica [tabla_freq()] a varias columnas
+#'
+#' @param data frame
+#' @param nomvar vector con el nombre de la variable categórica (categorical variable)
+#' @param peso en caso hayan pesos (if there are weights)
+#' @param starts TRUE si se quiere aplicar sobre columnas que comparten los mismos carácteres al inicio
+#' @param na en caso se consideren valores perdidos ()
+#'
+#' @return data frame con la cantidad de casos (n) y porcentaje (prop)
+#' @export
+#'
+#' @examples
+#' tabla_freq_columnas(iris, "Sepal", starts = TRUE)
+#'
+#'
+tabla_freq_columnas <- function(data, nomvar, peso = NULL, starts = NULL, na = NULL){
+
+  if(!is.null(starts)){
+    nom <- names(data[, grep(paste0("^", nomvar), names(data), value = TRUE)])
+    names(nom) <- nom
+  }else{
+    nom <- nomvar
+    names(nom) <- nom
+  }
+
+  df2 <- lapply(nom, function(x) tabla_freq(data, x, peso = peso, na = na))
+
+  pega_lista(df2, "var")
+
+}
---
+++ R/tabla_freq_estrato.R
@@ -0,0 +1,44 @@
+#' Tabla de frecuencias segun estrato (Frequency table by group)
+#'
+#' Aplica [tabla_freq()] según estrato
+#'
+#' @param data data frame
+#' @param var variable categórica (categorical variable)
+#' @param grupo grupo o estrato
+#' @param peso en caso hayan pesos (if there are weights)
+#' @param compara útil para comparar los porcentajes según el grupo
+#' @param na en caso se consideren valores perdidos ()
+#'
+#' @return data frame con la cantidad de casos (n) y porcentaje (prop)
+#' @export
+#'
+#' @examples
+#' tabla_freq_estrato(mtcars, var = "gear", grupo = "carb")
+#'
+#'
+tabla_freq_estrato <- function(data, var, grupo, peso = NULL, compara = NULL, na = NULL){
+
+  if(length(grupo) == 1){
+    names(data)[names(data) == grupo] <- "group_temp"
+  }else{
+    data$group_temp <- apply(data[, grupo], 1, paste, collapse = "-")
+  }
+
+  df2 <- lapply(split(data, data[["group_temp"]]), function(x) tabla_freq(x, var, peso = peso, na = na))
+  #df3 <- pega_lista(df2, "estrato")
+  df3 <- dplyr::bind_rows(df2, .id = "estrato")
+
+  if(length(grupo) == 1){
+    names(df3)[names(df3) == 'estrato'] <- grupo
+  }else{
+    df3 <- data.frame(df3[1:3], do.call(rbind, strsplit(df3[["estrato"]], "-")))
+    names(df3) <- c(names(df3[1:3]), grupo)
+  }
+
+  if(length(grupo) == 1 & !is.null(compara)){ #util para comparar los %
+    df3 <- reshape(df3[-3], idvar = "opcion", timevar = grupo, direction = "wide")
+  }
+
+  return(df3)
+
+}
